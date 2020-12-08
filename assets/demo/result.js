a54edcd253dc32de0ceb5fb15c39623f20c0cf4c
Trinh Duy Hai <36527583+Pkdarius@users.noreply.github.com>
2020-12-08 20:41:57 +0700

    done

--- assets/demo/result.js
+++ assets/demo/result.js
@@ -18,22 +18,46 @@ $(function () {
     const numOfMorbidity = urlParameters.get("numOfMorbidity");
     const familyHistory = urlParameters.get("familyHistory");
 
+    $('#recalculate').attr('href', `./index.html?gender=${gender}&maritalStatus=${maritalStatus}&education=${education}&age=${age}&height=${height}&weight=${weight}&smokingStatus=${smokingStatus}&drinkingStatus=${drinkingStatus}&physicalActivity=${physicalActivity}&redMeat=${redMeat}&tea=${tea}&coffee=${coffee}&numOfMorbidity=${numOfMorbidity}&familyHistory=${familyHistory}`)
+
     const bmi = weight / ((height / 100) * (height / 100));
-    const xigma = 0.1004906 - 0.0031462 * age + 0.1225548 * gender
+    const sigma = 0.1004906 - 0.0031462 * age + 0.1225548 * gender
         + 0.0291978 * maritalStatus - 0.0375267 * education + 0.026224 * bmi
         + 0.0406323 * smokingStatus + 0.1206483 * drinkingStatus
         + 0.1201688 * physicalActivity - 0.0772327 * familyHistory
         + 0.4353487 * numOfMorbidity - 0.7580442 * redMeat
         - 0.1374505 * tea - 0.2713213 * coffee;
-    const result = Math.round(100 / (1 + Math.exp(-xigma)));
-
-    const dataPreferences = {
-        series: [result, 100 - result]
-    };
-
-    const optionsPreferences = {
-        height: '230px'
-    };
-
-    Chartist.Pie('#chartPreferences', dataPreferences, optionsPreferences);
+    const result = Math.round(100 / (1 + Math.exp(-sigma)));
+    $('#result-text').text(result + '%');
+    const config = {
+        type: 'pie',
+        data: {
+            datasets: [{
+                labels: [
+                    'risk of cancer', 'abc'
+                ],
+                data: [
+                    result, 100 - result
+                ],
+                backgroundColor: [
+                    window.chartColors.red,
+                    window.chartColors.blue,
+                ]
+            }]
+        },
+        options: {
+            responsive: true,
+            legend: {
+                display: false
+            },
+            title: {
+                display: false
+            },
+            tooltips: {
+                enabled: false
+            }
+        }
+    }
+    const ctx = document.getElementById('chartPreferences').getContext('2d');
+    window.myPie = new Chart(ctx, config);
 });
